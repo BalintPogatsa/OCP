44e88e2e1c66a6eb6bdfd782a7755f2238583ba9
Balint Pogatsa <bp@macrix.pl>
2025-05-21 23:31:43 +0200

    added OCP example

---
+++ OCP.ConsoleApp/Button.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP.ConsoleApp
+{
+  public class Button
+  {
+    public ContentType Type { get; set; }
+    public string Content { get; set; }
+
+    public string RenderAsXml()
+    {
+      if (Type == ContentType.Text)
+      {
+        return $"<button><text>{Content}</text></button>";
+      }
+      else if (Type == ContentType.Image)
+      {
+        return $"<button><img src=\"{Content}\" /></button>";
+      }
+      else
+      {
+        throw new NotSupportedException("Unsupported button type.");
+      }
+    }
+  }
+}
---
+++ OCP.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OCP.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var textButton = new Button() { Type = ContentType.Text, Content = "Click Me" };
+            Console.WriteLine(textButton.RenderAsXml());
+        }
+    }
+}
